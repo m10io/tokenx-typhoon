d6c44daf65d91471e50c0ea95fc620156c4f6d2f
Sam Crang <sam.crang@gmail.com>
2018-11-06 13:40:33 +0000

    wip: whatever m8 just do all the ecr permissions

--- aws/container-linux/kubernetes/workers/iam.tf
+++ aws/container-linux/kubernetes/workers/iam.tf
@@ -21,15 +21,7 @@ data "aws_iam_policy_document" "worker_policy_doc" {
     actions = [
       "ec2:DescribeInstances",
       "ec2:DescribeRegions",
-      "ecr:CreateRepository",
-      "ecr:GetAuthorizationToken",
-      "ecr:BatchCheckLayerAvailability",
-      "ecr:GetDownloadUrlForLayer",
-      "ecr:GetRepositoryPolicy",
-      "ecr:DescribeRepositories",
-      "ecr:InitiateLayerUpload",
-      "ecr:ListImages",
-      "ecr:BatchGetImage",
+      "ecr:*"
     ]
 
     resources = ["*"]
